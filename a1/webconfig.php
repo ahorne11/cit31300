b9fe48a2c61dc82ca7a25910fec861148e426836
Aaron Horne <ahorne1196@gmail.com>
2017-09-10 23:47:39 -0400

    added absolute path

--- a1/webconfig.php
+++ a1/webconfig.php
@@ -1,5 +1,5 @@
 <?php
 
-define('ABSOLUTE_PATH', '');
+define('ABSOLUTE_PATH', '/home/aahorne/htdocs/a1');
 define('LOCAL_ROOT', 'http://corsair.cs.iupui.edu:21901/a1');
 define('URL_ROOT', 'http://corsair.cs.iupui.edu:21901/a1');
